20a79b3963ed94e3f1a04be00ba7dfbf52239c55
jakinpilla <jakinpilla@gmail.com>
2018-07-13 08:01:10 +0900

    first commit

---
+++ data_science_handbook_prac_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 12 08:09:18 2018
+
+@author: dsc
+"""
+
+from os import getcwd, chdir
+getcwd()
+chdir('C:/Users/dsc/data_science_handbook')
+
+import math
+math.sin(2)
+math.cos(2)
+
+print(In)
+Out
