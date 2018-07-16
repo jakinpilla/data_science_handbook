f4b5f8c47ef0155fc7adb64a5abf7a971c28a31b
jakinpilla <jakinpilla@gmail.com>
2018-07-16 17:53:09 +0900

    prac_3 modified

--- data_science_handbook_prac_3.py
+++ data_science_handbook_prac_3.py
@@ -117,18 +117,103 @@ ax.set(xlim=(0,10), ylim=(-2, 2),
        xlabel= 'x', ylabel='sin(x)', 
        title='A Sample Plot')
 
+# scatter plot
+x = np.linspace(0, 10, 30)
+y = np.sin(x)
+plt.plot(x, y, 'o', color='black')
 
+rng = np.random.RandomState(0)
+for marker in ['o', '.', ',', 'x', '+', 'o', '^', '<', '>', 's', 'd']:
+    plt.plot(rng.rand(5), rng.rand(5), marker, label="marker='{0}'".format(marker))
+    plt.legend(numpoints=1)
+    plt.xlim(0, 1.8)
 
+plt.plot(x, y, '-ok')
 
+plt.plot(x, y, '-p', color='gray', 
+         markersize=15, linewidth=4, 
+         markerfacecolor='white', markeredgecolor='gray',
+         markeredgewidth=2)
+plt.ylim(-1.2, 1.2)
 
+plt.scatter(x, y, marker='o')
 
+rng = np.random.RandomState(0)
+x = rng.randn(100)
+y = rng.randn(100)
+colors = rng.rand(100)
+sizes = 1000*rng.rand(100)
+plt.scatter(x, y, c=colors, s=sizes, alpha=.3, cmap='viridis')
+plt.colorbar()
 
+from sklearn.datasets import load_iris
+iris = load_iris()
+features = iris.data.T
+plt.scatter(features[0], features[1], alpha=.2, s=100*features[3], 
+            c=iris.target, cmap='viridis')
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1]);
 
+x = np.linspace(0,10,50)
+dy=0.8
+y = np.sin(x) + dy*np.random.randn(50)
+plt.errorbar(x, y, yerr=dy, fmt='.k')
+plt.errorbar(x, y, yerr=dy, fmt='o', color='black', 
+             ecolor='lightgray', elinewidth=3, capsize=0);
 
+from sklearn.gaussian_process import GaussianProcess
+model = lambda x : x*np.sin(x)
+xdata = np.array([1,3,5,6,8])
+ydata = model(xdata)
 
+gp=GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4, thetaU=1E-1, 
+                   random_start=100)
+gp.fit(xdata[:, np.newaxis], ydata)
+xfit = np.linspace(0, 10, 1000)
+yfit, MSE = gp.predict(xfit[:, np.newaxis], eval_MSE=True)
+dyfit = 2*np.sqrt(MSE)
 
+plt.plot(xdata, ydata, 'or')
+plt.plot(xfit, yfit, '-', color='gray')
+plt.fill_between(xfit, yfit - dyfit, yfit+dyfit, color='gray', alpha=.2)
+plt.xlim(0,10);
 
+def f(x,y):
+    return np.sin(x)**10 + np.cos(10+y*x)*np.cos(x)
 
+x = np.linspace(0, 5, 50)
+y = np.linspace(0, 5, 40)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+plt.contour(X, Y, Z, colors='black')
+
+plt.contour(X, Y, Z, 20, cmap='RdGy');
+
+plt.contourf(X, Y, Z, 20, cmap='RdGy')
+plt.colorbar()
+
+plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy')
+plt.colorbar()
+plt.axis(aspect='image');
+
+contours = plt.contour(X, Y, Z, 3, colors='black')
+plt.clabel(contours, inline=True, fontsize=8)
+plt.imshow(Z, extent=[0, 5, 0, 5], origin='lower', cmap='RdGy', alpha=.5)
+plt.colorbar();
+
+data= np.random.randn(1000)
+plt.hist(data);
+plt.hist(data, bins=30, normed=True, alpha=.5, histtype='stepfilled', 
+         color='steelblue', edgecolor='none')
+x1 = np.random.normal(0, 0.8, 1000)
+x2 = np.random.normal(-2, 1, 1000)
+x3 = np.random.normal(3, 2, 1000)
+
+kwargs = dict(histtype='stepfilled', alpha=.3, normed=True, bins=40)
+plt.hist(x1, **kwargs)
+plt.hist(x2, **kwargs)
+plt.hist(x3, **kwargs)
 
 
 
